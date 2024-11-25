6396477bbca7294603aae79dae192eb8b34d8308
johann681 <adekolaolaonipekun@gmail.com>
2024-11-25 13:29:44 +0100

    first commit

---
+++ assingnments.js
@@ -0,0 +1,70 @@
+// week 1
+let length = 10;
+let width = 3;
+
+// calculating area
+let area = length * width;
+console.log(area);
+console.log(length);
+console.log(width);
+
+// 2
+
+let adekola = {
+    firstName:"Adekola",
+    lastName:"Daemon",
+    age:"18",
+    student:"true"
+};
+
+console.log(adekola);
+// 3
+
+let movies = {
+    2021:"the mummy",
+    2022:"incredibly wicked",
+    2023:"moon kinght",
+    2024:"stand alone complex",
+    2025:"unkown"
+};
+console.log(movies);
+
+let books = {
+    author:"Batman",
+    Title:"i am wealth",
+    year:2024,
+    genra:{
+        death:"yep",
+        life:"nope",
+        midd:"ehh"
+    },
+    available:"true"
+};
+
+console.log(books)
+
+
+
+
+
+// week2
+let a = 12;
+let b = 13;
+let c = 14;
+let d = 15;
+
+
+console.log(a < 16 && b > a);
+// double && stands for and!! so in this situation where a is 12 which is less than 16 and b which is 13 is grater than a which is 12 is true
+
+
+console.log(a == b || b == 13);
+// double || stands for or!! so in this situation we are saying a = b or b = 13 which one of them is true
+
+console.log (a != 13);
+// the ! stands for not equal to 
+
+
+var lastName = "adekola";
+var lastNam = [lastName.length - [1]];
+console.log(lastName.length);
